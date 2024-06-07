de16658bb1350803ba3a8f7b3183f174c29e3f5e
Nyuhei <nyuhei06@gmail.com>
2024-06-08 00:11:11 +0900

    ドイツを追加

--- history/000_state.py
+++ history/000_state.py
@@ -1,14 +1,8 @@
 import pathlib
-import csv
 import pandas as pd
 input_dir = 'history/states'
-# 保存するファルダ
-output_dir = './'
-
 # フォルダ内に保存されているファイル一覧
 state_list = list(pathlib.Path(input_dir).glob('**/*.txt'))
-
-# print(state_list)
 for i in range (len(state_list)):
 
     with open(state_list[i],"r",encoding="utf-8") as f:
