504e347cb374b20c24d4b370c182ae2cae99c533
Nyuhei <nyuhei06@gmail.com>
2024-06-07 22:14:33 +0900

    全州中核、所有者削除

--- history/000_state.py
+++ history/000_state.py
@@ -10,4 +10,16 @@ state_list = list(pathlib.Path(input_dir).glob('**/*.txt'))
 
 # print(state_list)
 for i in range (len(state_list)):
-    pd.read_table(state_list[i],encoding="utf_8")
+
+    with open(state_list[i],"r",encoding="utf-8") as f:
+        df = pd.Series(f.readlines())
+        print(df)
+        df.drop(df[df.str.contains('owner')].index,inplace=True)
+        df.drop(df[df.str.contains('add_core_of')].index,inplace=True)
+        lines = df.to_list()
+
+    with open(state_list[i],"w",encoding="utf-8") as f:
+        
+        f.write("")
+        for j in range(len(lines)):
+            f.write(str(lines[j]))
